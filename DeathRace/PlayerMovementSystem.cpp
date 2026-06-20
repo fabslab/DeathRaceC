96374ec5a14ee610072b5ea4a2bb4c593e4029a9
Fabien <fabienbrooke@gmail.com>
2026-06-19 17:31:46 -0700

    refactor: replace C-style arrays with std::array and add nullptr init

--- DeathRace/PlayerMovementSystem.cpp
+++ DeathRace/PlayerMovementSystem.cpp
@@ -20,12 +20,183 @@ void PlayerMovementSystem::configure(ECS::World* world)
 void PlayerMovementSystem::unconfigure(ECS::World* world)
 {
     world->unsubscribeAll(this);
-    delete keyboardInputLeft;
-    delete keyboardInputRight;
-    delete controllerInputOne;
-    delete controllerInputTwo;
+#include "PlayerMovementSystem.h"
+#include "Components.h"
+#include "GameAudio.h"
+#include "GameConstants.h"
+#include "GameStateChangedEventSubscriber.h"
+#include "MathUtil.h"
+#include "raymath.h"
+#include <algorithm>
+
+void PlayerMovementSystem::configure(ECS::World* world)
+{
+    world->subscribe<Events::CollisionEnteredEvent>(this);
+    world->subscribe<Events::NumberOfPlayersChanged>(this);
+    keyboardInputLeft = new KeyboardPlayerInput(Input::PLAYER_LEFT);
+    keyboardInputRight = new KeyboardPlayerInput(Input::PLAYER_RIGHT);
+    controllerInputOne = new ControllerPlayerInput(PlayerIndex::One);
+    controllerInputTwo = new ControllerPlayerInput(PlayerIndex::Two);
+}
+
+void PlayerMovementSystem::unconfigure(ECS::World* world)
+{
+    world->unsubscribeAll(this);
+    if (keyboardInputLeft) { delete keyboardInputLeft; keyboardInputLeft = nullptr; }
+    if (keyboardInputRight) { delete keyboardInputRight; keyboardInputRight = nullptr; }
+    if (controllerInputOne) { delete controllerInputOne; controllerInputOne = nullptr; }
+    if (controllerInputTwo) { delete controllerInputTwo; controllerInputTwo = nullptr; }
     for (auto& sound : playerEngineSounds) {
         if (IsMusicReady(sound)) {
+            StopMusicStream(sound);
+            UnloadMusicStream(sound);
+            sound = Music{};
+        }
+    }
+}
+
+void PlayerMovementSystem::receive(ECS::World* world, const Events::CollisionEnteredEvent& event)
+{
+    auto firstEntity = world->getById(event.firstEntity);
+    auto secondEntity = world->getById(event.secondEntity);
+    if (!firstEntity || !secondEntity) {
+        return;
+    }
+    auto playerMovementComponent = firstEntity->get<Components::PlayerMovementComponent>();
+    if (playerMovementComponent) {
+        if (!secondEntity->get<Components::EnemyMovementComponent>()) {
+            CrashPlayer(firstEntity);
+        }
+    }
+}
+
+void PlayerMovementSystem::receive(ECS::World* world, const Events::NumberOfPlayersChanged& event)
+{
+    numPlayers = event.numberOfPlayers;
+}
+
+void PlayerMovementSystem::tick(ECS::World* world, float deltaTime)
+{
+    auto gameState = GameStateChangedEventSubscriber::GetGameState();
+    if (gameState != GameState::GameRunning) {
+        return;
+    }
+
+    // Play a single idle sound even if two players, two sounds together sounds too overwhelming for an idle sound.
+    UpdateEngineIdleSound();
+
+    world->each<Components::PlayerMovementComponent, Components::SnappedRotationComponent, Components::Transform2DComponent>(
+        [&](ECS::Entity* entity,
+            ECS::ComponentHandle<Components::PlayerMovementComponent> movementComponent,
+            ECS::ComponentHandle<Components::SnappedRotationComponent> snappedRotationComponent,
+            ECS::ComponentHandle<Components::Transform2DComponent> transformComponent) {
+            if (numPlayers == 1) {
+                inputAggregator.SetInputs({ keyboardInputLeft, keyboardInputRight, controllerInputOne, controllerInputTwo });
+            } else if (numPlayers == 2) {
+                if (movementComponent->playerIndex == PlayerIndex::One) {
+                    inputAggregator.SetInputs({ keyboardInputLeft, controllerInputOne });
+                } else if (movementComponent->playerIndex == PlayerIndex::Two) {
+                    inputAggregator.SetInputs({ keyboardInputRight, controllerInputTwo });
+                }
+            }
+
+            float throttle = inputAggregator.GetThrottleValue();
+            float turnDirection = inputAggregator.GetDirection();
+
+            UpdateEngineRunningSound(movementComponent->playerIndex, throttle);
+
+            movementComponent->remainingCrashTime = std::max(movementComponent->remainingCrashTime - deltaTime, 0.f);
+            if (throttle < 0.f) {
+                turnDirection = -turnDirection;
+                movementComponent->remainingCrashTime = 0.f;
+            }
+
+            if (movementComponent->remainingCrashTime <= 0.f) {
+                float rotation = transformComponent->rotation;
+                if (turnDirection == 0.f) {
+                    // Reset rotation back to snapped value if player stops turning
+                    rotation = MathUtil::Snap(rotation, snappedRotationComponent->snapAngle);
+                } else {
+                    rotation += (turnDirection * snappedRotationComponent->maxTurnAnglePerUpdate);
+                    // Prevent value from becoming too large by resetting once full circle reached
+                    float pi2 = PI * 2;
+                    if (rotation > pi2) {
+                        rotation -= pi2;
+                    } else if (rotation < -pi2) {
+                        rotation += pi2;
+                    }
+                }
+                transformComponent->rotation = rotation;
+
+                if (throttle != 0.f) {
+                    float speed = 0.f;
+                    if (throttle > 0.f) {
+                        auto collisionComponent = entity->get<Components::CollisionComponent>();
+                        if (collisionComponent && !collisionComponent->currentCollisions.empty()) {
+                            speed = throttle * movementComponent->speedWhileColliding;
+                        } else {
+                            speed = throttle * movementComponent->forwardSpeed;
+                        }
+                    } else {
+                        speed = throttle * movementComponent->reverseSpeed;
+                    }
+                    float snappedRotation = MathUtil::Snap(rotation, snappedRotationComponent->snapAngle);
+                    Matrix rotationMatrix = MatrixRotateZ(-snappedRotation);
+                    Vector3 movementDirection = Vector3Transform(MathUtil::Vector2To3(DirectionVectors::Down), rotationMatrix);
+                    Vector3 movement = Vector3Scale(movementDirection, speed);
+                    transformComponent->position = MathUtil::WrapPosition(
+                        Vector2Add(transformComponent->position, MathUtil::Vector3To2(movement)),
+                        GameConstants::GAME_BOUNDS);
+                }
+            }
+        });
+}
+
+void PlayerMovementSystem::UpdateEngineIdleSound()
+{
+    if (!IsMusicReady(engineIdleSound)) {
+        engineIdleSound = LoadMusicStream("Content/Audio/engine-idle-loop.ogg");
+        PlayMusicStream(engineIdleSound);
+    }
+    UpdateMusicStream(engineIdleSound);
+}
+
+void PlayerMovementSystem::UpdateEngineRunningSound(PlayerIndex playerIndex, float throttle)
+{
+    int engineSoundIndex = static_cast<int>(playerIndex);
+    Music& engineSound = playerEngineSounds[engineSoundIndex];
+
+    if (!IsMusicReady(engineSound)) {
+        engineSound = LoadMusicStream("Content/Audio/engine-running-loop.ogg");
+    }
+
+    UpdateMusicStream(engineSound);
+
+    if (throttle != 0.f) {
+        if (!IsMusicStreamPlaying(engineSound)) {
+            PlayMusicStream(engineSound);
+        }
+        float volume = 1.f;
+        playerEngineVolumes[engineSoundIndex] = volume;
+        SetMusicVolume(engineSound, volume);
+    } else {
+        if (playerEngineVolumes[engineSoundIndex] < 0.1f) {
+            StopMusicStream(engineSound);
+        } else {
+            playerEngineVolumes[engineSoundIndex] -= 0.1f;
+            SetMusicVolume(engineSound, playerEngineVolumes[engineSoundIndex]);
+        }
+    }
+}
+
+void PlayerMovementSystem::CrashPlayer(ECS::Entity* player)
+{
+    auto playerMovementComponent = player->get<Components::PlayerMovementComponent>();
+    if (playerMovementComponent->remainingCrashTime <= 0.f) {
+        PlaySound(GameAudio::collision);
+        playerMovementComponent->remainingCrashTime = playerMovementComponent->crashTime;
+    }
+}
             StopMusicStream(sound);
             UnloadMusicStream(sound);
             sound = Music{};
--- DeathRace/PlayerMovementSystem.h
+++ DeathRace/PlayerMovementSystem.h
@@ -26,10 +26,46 @@ private:
     void UpdateEngineRunningSound(PlayerIndex playerIndex, float throttle);
     void CrashPlayer(ECS::Entity* player);
     AggregatedPlayerInput inputAggregator;
-    KeyboardPlayerInput *keyboardInputLeft, *keyboardInputRight;
-    ControllerPlayerInput *controllerInputOne, *controllerInputTwo;
+    KeyboardPlayerInput* keyboardInputLeft = nullptr;
+    KeyboardPlayerInput* keyboardInputRight = nullptr;
+    ControllerPlayerInput* controllerInputOne = nullptr;
+    ControllerPlayerInput* controllerInputTwo = nullptr;
+    int numPlayers = 0;
+#pragma once
+
+#include "AggregatedPlayerInput.h"
+#include "ControllerPlayerInput.h"
+#include "ECS.h"
+#include "Events.h"
+#include "KeyboardPlayerInput.h"
+#include "PlayerIndex.h"
+#include "raylib.h"
+#include <array>
+#include <vector>
+
+class PlayerMovementSystem
+    : public ECS::EntitySystem,
+      public ECS::EventSubscriber<Events::CollisionEnteredEvent>,
+      public ECS::EventSubscriber<Events::NumberOfPlayersChanged> {
+public:
+    void configure(ECS::World* world) override;
+    void unconfigure(ECS::World* world) override;
+    void receive(ECS::World* world, const Events::CollisionEnteredEvent& event) override;
+    void receive(ECS::World* world, const Events::NumberOfPlayersChanged& event) override;
+    void tick(ECS::World* world, float deltaTime) override;
+
+private:
+    void UpdateEngineIdleSound();
+    void UpdateEngineRunningSound(PlayerIndex playerIndex, float throttle);
+    void CrashPlayer(ECS::Entity* player);
+    AggregatedPlayerInput inputAggregator;
+    KeyboardPlayerInput* keyboardInputLeft = nullptr;
+    KeyboardPlayerInput* keyboardInputRight = nullptr;
+    ControllerPlayerInput* controllerInputOne = nullptr;
+    ControllerPlayerInput* controllerInputTwo = nullptr;
     int numPlayers = 0;
     Music engineIdleSound = {};
-    Music playerEngineSounds[2] = { {}, {} };
-    float playerEngineVolumes[2] = { 0.f, 0.f };
+    std::array<Music, 2> playerEngineSounds = { {}, {} };
+    std::array<float, 2> playerEngineVolumes = { 0.f, 0.f };
+};
 };
